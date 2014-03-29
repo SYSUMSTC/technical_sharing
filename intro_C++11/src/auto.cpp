fca8a7c3c19e89788186547276e33da2bdc03f92
chenzy <chenzeyuczy@qq.com>
2014-03-29 12:01:25 +0800

    Intro to C++11

---
+++ intro_C++11/src/auto.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	auto myInt = 0;  // This is a int
+	auto myDouble = 3.14;  // This is a double
+	auto myString = "nothing";  // This is a string
+
+	cout << "My int is " << myInt << endl;
+	cout << "My double is " << myDouble << endl;
+	cout << "My string is " << myString << endl;
+
+	return 0;
+}
